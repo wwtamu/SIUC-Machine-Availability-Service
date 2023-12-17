d3245920ae8de7835b45b0ad46b064faa757f1eb
William Welling <wwelling@tamu.edu>
2023-12-17 14:23:08 -0600

    blast from the past

---
+++ SIUC Machine Availability Service/IService1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SIUCMachineAvailabilityService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        bool Login(string machine_name);
+
+        [OperationContract]
+        bool Logout(string machine_name);
+
+        [OperationContract]
+        bool Reset(string machine_name);
+
+        [OperationContract]
+        bool Initialize(string machine_name, string mac_address, string ip_address);
+
+        [OperationContract]
+        List<bool> GetComputersAvailability(string lab_name);
+
+        [OperationContract]
+        List<string> GetComputersStatus(string lab_name);
+
+        [OperationContract]
+        bool GetLabAvailability(string lab_name);
+        
+        [OperationContract]
+        List<LabObject> GetAllLabs();
+
+        [OperationContract]
+        List<MachineObject> GetComputersInLab(string lab_name);
+    }
+
+    [DataContract]
+    public class EventObject
+    {                                           // Microsoft SQL dbo.Event
+        private int event_id;                   // eid   int primary key NOT NULL
+        private DateTime date_time;             // edate datetime NOT NULL
+        private int machine_id;                 // fmid  int references dbo.Machines(mid) NOT NULL
+        private string e_string;                // event varchar(10) {"login", "logoff", "reset"}        
+
+        [DataMember]
+        public int EventId
+        {
+            get { return event_id; }
+            set { event_id = value; }
+        }
+        [DataMember]
+        public DateTime EventDateTime
+        {
+            get { return date_time; }
+            set { date_time = value; }
+        }
+        [DataMember]
+        public int MachineId
+        {
+            get { return machine_id; }
+            set { machine_id = value; }
+        }
+        [DataMember]
+        public string EventString
+        {
+            get { return e_string; }
+            set { e_string = value; }
+        }
+    }
+
+    [DataContract]
+    public class AvailabilityObject
+    {                                           // Microsoft SQL dbo.Machines
+        private int availability_id;            // aid   int primary key NOT NULL
+        private DateTime date_time;             // adate datetime NOT NULL
+        private int machine_id;                 // fmid  int references dbo.Machines(mid) NOT NULL
+        private string machine_last_known_ip;   // mtype varchar(15)
+        private bool available;                 // mavailable bit NOT NULL
+
+        [DataMember]
+        public int AvailabilityId
+        {
+            get { return availability_id; }
+            set { availability_id = value; }
+        }
+        [DataMember]
+        public DateTime AvailabilityDateTime
+        {
+            get { return date_time; }
+            set { date_time = value; }
+        }
+        [DataMember]
+        public int MachineId
+        {
+            get { return machine_id; }
+            set { machine_id = value; }
+        }
+        [DataMember]
+        public string MachineLastKnownIPAddress
+        {
+            get { return machine_last_known_ip; }
+            set { machine_last_known_ip = value; }
+        }
+        [DataMember]
+        public bool Availability
+        {
+            get { return available; }
+            set { available = value; }
+        }
+    }
+
+    [DataContract]
+    public class MachineObject
+    {                                   // Microsoft SQL dbo.Machines
+        private int machine_id;         // mid   int primary key NOT NULL
+        private int lab_id;             // flid  int references dbo.Labs(lid) NOT NULL        
+        private string machine_name;    // mname varchar(100) NOT NULL
+        private string machine_mac;     // mmac varchar(20) NOT NULL
+        private string machine_lkip;    // mlkip varchar(20) NOT NULL
+        private string machine_label;   // mlabel varchar(20)
+        private string machine_type;    // mtype varchar(50)
+
+        [DataMember]
+        public int MachineId
+        {
+            get { return machine_id; }
+            set { machine_id = value; }
+        }
+        [DataMember]
+        public int LabId
+        {
+            get { return lab_id; }
+            set { lab_id = value; }
+        }
+        [DataMember]
+        public string MachineName
+        {
+            get { return machine_name; }
+            set { machine_name = value; }
+        }
+        [DataMember]
+        public string MachineMac
+        {
+            get { return machine_mac; }
+            set { machine_mac = value; }
+        }
+        [DataMember]
+        public string MachineIP
+        {
+            get { return machine_lkip; }
+            set { machine_lkip = value; }
+        }
+        [DataMember]
+        public string MachineLabel
+        {
+            get { return machine_label; }
+            set { machine_label = value; }
+        }
+        [DataMember]
+        public string MachineType
+        {
+            get { return machine_type; }
+            set { machine_type = value; }
+        }
+    }
+
+    [DataContract]
+    public class LabObject
+    {                                   // Microsoft SQL dbo.Labs
+        private int lab_id;             // lid    int primary key NOT NULL
+        private string lab_name;        // lname  varchar(100)
+        private string lab_building;    // lbuilding varchar(100) NOT NULL
+        private int lab_floor;          // lfloor int 
+        private string lab_room;        // lroom  varchar(20)
+        private string lab_open;        // lopen  time NOT NULL
+        private string lab_close;       // lclose time NOT NULL
+        private string lab_status;      // lstat  varchar(10) NOT NULL
+        private int machine_count;      // mcount int NOT NULL
+
+        [DataMember]
+        public int LabId
+        {
+            get { return lab_id; }
+            set { lab_id = value; }
+        }
+        [DataMember]
+        public string LabName
+        {
+            get { return lab_name; }
+            set { lab_name = value; }
+        }
+        [DataMember]
+        public string LabBuilding
+        {
+            get { return lab_building; }
+            set { lab_building = value; }
+        }
+        [DataMember]
+        public int LabFloor
+        {
+            get { return lab_floor; }
+            set { lab_floor = value; }
+        }
+        [DataMember]
+        public string LabRoom
+        {
+            get { return lab_room; }
+            set { lab_room = value; }
+        }
+        [DataMember]
+        public string LabOpen
+        {
+            get { return lab_open; }
+            set { lab_open = value; }
+        }
+        [DataMember]
+        public string LabClose
+        {
+            get { return lab_close; }
+            set { lab_close = value; }
+        }
+        [DataMember]
+        public string LabStatus
+        {
+            get { return lab_status; }
+            set { lab_status = value; }
+        }
+        [DataMember]
+        public int LabMachineCount
+        {
+            get { return machine_count; }
+            set { machine_count = value; }
+        }
+    }
+}
---
+++ SIUC Machine Availability Service/SIUCMachineAvailabilityService.svc.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SIUCMachineAvailabilityService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class SIUCMacineAvailabilityService : IService1
+    {
+        public bool Initialize(string machine_name, string mac_address, string ip_address)
+        {
+            bool success = false;
+            using (machinesEntities MachineData = new machinesEntities())
+            {
+                var query = from m in MachineData.Machines
+                            where m.mname == machine_name
+                            select m;
+
+                DateTime now = DateTime.Now;
+                success = InsertEvent(query.FirstOrDefault().mid, "initialized");
+                query.FirstOrDefault().mmac = mac_address;
+                query.FirstOrDefault().mlkip = ip_address;
+                // Submit the changes to the database.
+                try
+                {
+                    MachineData.SaveChanges();
+                    success = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    // Provide for exceptions.
+                    success = false;
+                }
+            }
+            return success;
+        }
+
+        public bool Login(string machine_name)
+        {
+            bool success = false;
+            using (machinesEntities MachineData = new machinesEntities()) {
+                var query = from a in MachineData.Availabilities
+                            join m in MachineData.Machines
+                            on a.fmid equals m.mid
+                            where m.mname == machine_name
+                            select a;
+
+                DateTime now = DateTime.Now;
+                if ((now - query.FirstOrDefault().adate).TotalSeconds > 5) {
+                    success = InsertEvent(query.FirstOrDefault().fmid, "login");
+                    query.FirstOrDefault().mavailable = false;
+                }
+                query.FirstOrDefault().adate = now;
+                // Submit the changes to the database.
+                try {
+                    MachineData.SaveChanges();
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                    // Provide for exceptions.
+                    success = false;
+                }
+            }
+            return success;
+        }
+
+        public bool Logout(string machine_name)
+        {
+            bool success = false;
+            using (machinesEntities MachineData = new machinesEntities()) {
+                var query = from a in MachineData.Availabilities
+                            join m in MachineData.Machines
+                            on a.fmid equals m.mid
+                            where m.mname == machine_name
+                            select a;
+
+                DateTime now = DateTime.Now;
+                if ((now - query.FirstOrDefault().adate).TotalSeconds > 5) {
+                    success = InsertEvent(query.FirstOrDefault().fmid, "logout");
+                    query.FirstOrDefault().mavailable = true;
+                }
+                query.FirstOrDefault().adate = now;
+                // Submit the changes to the database.
+                try {
+                    MachineData.SaveChanges();
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                    // Provide for exceptions.
+                    success = false;
+                }
+            }
+            return success;
+        }
+
+        public bool Reset(string machine_name) {
+            bool success = false;
+            using (machinesEntities MachineData = new machinesEntities()) {
+                var query = from a in MachineData.Availabilities
+                            join m in MachineData.Machines
+                            on a.fmid equals m.mid
+                            where m.mname == machine_name
+                            select a;
+
+                DateTime now = DateTime.Now;
+                if ((now - query.FirstOrDefault().adate).TotalSeconds > 5) {
+                    success = InsertEvent(query.FirstOrDefault().fmid, "reset");
+                    query.FirstOrDefault().mavailable = true;
+                }
+                query.FirstOrDefault().adate = now;
+                // Submit the changes to the database.
+                try {
+                    MachineData.SaveChanges();
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                    // Provide for exceptions.
+                    success = false;
+                }
+            }
+            return success;
+        }
+
+        public bool InsertEvent(int fmid, string estring)
+        {
+            machinesEntities MachineData = new machinesEntities();
+
+            Event NewEvent = new Event();
+            
+            NewEvent.fmid = fmid;
+            NewEvent.edate = DateTime.Now;
+            NewEvent.estring = estring;
+
+            MachineData.Events.Add(NewEvent);
+
+            // Submit the changes to the database.
+            try
+            {
+                MachineData.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                // Provide for exceptions.
+            }
+            return false;
+        }
+
+        public List<LabObject> GetAllLabs()
+        {
+            var labs = new List<LabObject>();
+            
+            using (machinesEntities MachineData = new machinesEntities())
+            {
+                var query = from l in MachineData.Labs
+                            select l;
+
+                foreach (var lab in query)
+                {
+                    LabObject new_lab = new LabObject();
+
+                    new_lab.LabId = lab.lid;
+
+                    new_lab.LabName = lab.lname;
+                    new_lab.LabBuilding = lab.lbuilding;
+                    new_lab.LabFloor = (int)lab.lfloor;
+                    new_lab.LabRoom = lab.lroom;
+                    new_lab.LabOpen = lab.lopen.ToString();
+                    new_lab.LabClose = lab.lclose.ToString();
+                    new_lab.LabStatus = lab.lstat;
+                    new_lab.LabMachineCount = lab.mcount;
+                    
+                    labs.Add(new_lab);
+                }
+            }
+
+            return labs;
+        }
+
+        public List<MachineObject> GetComputersInLab(string lab_name)
+        {
+            var computers = new List<MachineObject>();
+
+            using (machinesEntities MachineData = new machinesEntities())
+            {
+                var query = from m in MachineData.Machines
+                            join l in MachineData.Labs
+                            on m.flid equals l.lid
+                            where l.lname == lab_name
+                            select m;
+
+                foreach (var machine in query)
+                {
+                    MachineObject new_machine = new MachineObject();
+
+                    new_machine.MachineId = machine.mid;
+                    new_machine.MachineName = machine.mname;
+                    new_machine.MachineMac = machine.mmac;
+                    new_machine.MachineIP = machine.mlkip;
+                    new_machine.MachineLabel = machine.mlabel;
+                    new_machine.MachineType = machine.mtype;
+
+                    computers.Add(new_machine);
+                }
+            }
+
+            return computers;
+        }
+
+        public List<string> GetComputersStatus(string lab_name)
+        {
+            var StatusList = new List<string>();
+
+            using (machinesEntities MachineData = new machinesEntities())
+            {
+                var query = from m in MachineData.Machines
+                            join l in MachineData.Labs
+                            on m.flid equals l.lid
+                            where l.lname == lab_name
+                            select m;
+
+                foreach (var M in query)
+                {
+                    StatusList.Add(M.mstat);
+                }
+
+                return StatusList;
+            }
+        }
+
+        public List<bool> GetComputersAvailability(string lab_name)
+        {
+            var AvailabilityList = new List<bool>();
+
+            using (machinesEntities MachineData = new machinesEntities())
+            {
+                var query = from a in MachineData.Availabilities
+                            join m in MachineData.Machines
+                            on a.fmid equals m.mid
+                            join l in MachineData.Labs
+                            on m.flid equals l.lid
+                            where l.lname == lab_name
+                            select a;
+
+                foreach (var A in query)
+                {                    
+                    AvailabilityList.Add(A.mavailable);
+                }
+
+                return AvailabilityList;
+            }
+        }
+
+        public bool GetLabAvailability(string lab_name)
+        {
+            using (machinesEntities MachineData = new machinesEntities())
+            {
+                var query = from l in MachineData.Labs
+                            where l.lname == lab_name
+                            select l;
+
+                foreach (var A in query)
+                {
+                    if (A.lstat == "Open")
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
